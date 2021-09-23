e6a0e424904d2066e102d2a28c17864e17ef1a4c
Devrath <devrath.dev595@gmail.com>
2021-09-23 10:45:01 +0530

    v

---
+++ Application/inappreview/src/main/java/com/inappreview/di/InAppReviewBinds.kt
@@ -0,0 +1,26 @@
+package com.inappreview.di
+
+import com.inappreview.preferences.InAppReviewPreferences
+import com.inappreview.preferences.InAppReviewPreferencesImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Provides dependencies required for In App Review flow.
+ * */
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class InAppReviewBinds {
+
+  /**
+   * Provides Preferences wrapper.
+   * */
+  @Binds
+  @Singleton
+  abstract fun bindInAppReviewPreferences(
+    inAppReviewPreferencesImpl: InAppReviewPreferencesImpl
+  ): InAppReviewPreferences
+}

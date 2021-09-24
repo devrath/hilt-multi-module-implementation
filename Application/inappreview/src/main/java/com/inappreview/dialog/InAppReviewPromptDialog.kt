bd73b836d20be8d31f33552ad41164c432d2aa45
Devrath <devrath.dev595@gmail.com>
2021-09-24 17:13:08 +0530

    m

--- Application/inappreview/src/main/java/com/inappreview/dialog/InAppReviewPromptDialog.kt
+++ Application/inappreview/src/main/java/com/inappreview/dialog/InAppReviewPromptDialog.kt
@@ -1,15 +1,11 @@
 package com.inappreview.dialog
 
 import android.content.DialogInterface
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import com.inappreview.manager.InAppReviewManager
-import com.inappreview.inappreview.R
 import com.inappreview.inappreview.databinding.FragmentInAppReviewPromptBinding
 import com.inappreview.preferences.InAppReviewPrefsStoreImpl
 import com.inappreview.preferences.general.GeneralGeneralPrefsStoreImpl
@@ -20,20 +16,11 @@ import kotlinx.coroutines.Job
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
-import androidx.lifecycle.Lifecycle
-
-import androidx.annotation.NonNull
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.inappreview.inappreview.R
 import kotlinx.coroutines.launch
 
-
-/**
- * Shows a dialog that asks the user if they want to review the app.
- *
- * This dialog is shown only if the user hasn't previously rated the app, hasn't asked to never
- * rate the app or if they asked to rate it later and enough time passed (a week).
- * */
 @AndroidEntryPoint
 class InAppReviewPromptDialog : BottomSheetDialogFragment(), CoroutineScope {
 
@@ -58,8 +45,13 @@ class InAppReviewPromptDialog : BottomSheetDialogFragment(), CoroutineScope {
     savedInstanceState: Bundle?
   ): View? {
     binding = FragmentInAppReviewPromptBinding.inflate(inflater, container, false)
+    return binding?.root?.apply {
+      setBackgroundResource(R.drawable.rounded_background);
+    }
+  }
 
-    return binding?.root
+  override fun getTheme(): Int {
+    return R.style.CustomBottomSheetDialog
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -91,24 +83,6 @@ class InAppReviewPromptDialog : BottomSheetDialogFragment(), CoroutineScope {
   }
 
   /**
-   * Styles the dialog to have a transparent background and window insets.
-   * */
-  override fun onStart() {
-    super.onStart()
-    initStyle()
-  }
-
-  private fun initStyle() {
-    val back = ColorDrawable(Color.TRANSPARENT)
-    dialog?.window?.setBackgroundDrawable(back)
-
-    /*dialog?.window?.setLayout(
-      resources.getDimensionPixelSize(R.dimen.ratePromptWidth),
-      resources.getDimensionPixelSize(R.dimen.ratePromptHeight)
-    )*/
-  }
-
-  /**
    * If the user cancels the dialog, we process that as if they chose to "Rate Later".
    * */
   override fun onCancel(dialog: DialogInterface) {
@@ -124,4 +98,5 @@ class InAppReviewPromptDialog : BottomSheetDialogFragment(), CoroutineScope {
   private fun getLaterTime(): Long {
     return System.currentTimeMillis() + TimeUnit.DAYS.toMillis(14)
   }
+
 }
